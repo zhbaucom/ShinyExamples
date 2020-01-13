acb45b87bdd949c820cdfc19d2a7ca6d9012919d
Zach Baucom <zachbaucom24@gmail.com>
2020-01-13 11:19:04 -0500

    Updated App and Scraper

--- DemocraticPolicies/DemoNomWebScrape.R
+++ DemocraticPolicies/DemoNomWebScrape.R
@@ -2,6 +2,9 @@
 library(tidyverse)
 #Package for data scraping
 library(rvest)
+#For making nice tables
+library(knitr)
+library(kableExtra)
 
 #Grab all the html information from wikepedia
 url <- "https://en.wikipedia.org/wiki/Political_positions_of_the_2020_Democratic_Party_presidential_primary_candidates"
@@ -14,7 +17,10 @@ tbls <- webpage %>%
 
 #The html tables have a lot of references denote by "[...]" which are being removed
 tbls <- lapply(tbls, function(x){
-  data.frame(apply(x, 2, function(y){gsub("\\[.*?\\]", "", y)}))
+  data.frame(apply(x, 2, function(y){
+    y.out <- gsub("\\[.*?\\]", "", y)
+    gsub("\n","",y.out)
+  }))
 })
 #After using the apply function and retransforming to a data.frame it replaced 
 #all spaces in the column names with a ".". This the spaces back in
@@ -38,6 +44,11 @@ names(tbls) <- tblNames
 #Saves the list in a .RDS for quick upload into the shiny app
 saveRDS(tbls, "DemocraticNomineePolicies.RDS")
 
+#################################################################
+DemTabs <- tbls
+DemTabNames <- names(DemTabs)
+DemNames <- as.character(DemTabs[[1]]$Candidate)
+DemSR <- as.character(DemTabs[[1]]$`Still running `)
 
 #Transposing the tables and binding them all to 1 big table
 qbcList <- lapply(DemTabNames, function(x){
@@ -47,6 +58,24 @@ qbcList <- lapply(DemTabNames, function(x){
 })
 qbc <- do.call("rbind", qbcList)
 qbs <- qbc[-(grep("Still running", row.names(qbc))[-1]),]
+qbs$Category <- factor(qbs$Category, levels = c("Still Running", levels(qbs$Category)))
+qbs$Category[1] <- "Still Running"
+
+#Change the backgrounds conditionally
+colorFun <- function(x){
+  ifelse(grepl("Yes", x), "green",
+         ifelse(grepl("No", x), "red",
+                ifelse(grepl("Other", x), "grey", "white")))
+}
+
+#Convert table into html coded and change the background colors based on answers
+qbs <- qbs %>%
+  rownames_to_column(var = "Topic")%>%
+  mutate_at(vars(-Topic, -Category),function(x){
+    cell_spec(x, background = colorFun(as.character(x)))
+  }) 
 
 #Save the data
 saveRDS(qbs, "qbc.RDS")
+
+
--- DemocraticPolicies/DemoPolicyApp.R
+++ DemocraticPolicies/DemoPolicyApp.R
@@ -1,10 +1,12 @@
 library(shiny)
 library(DT)
+library(knitr)
 
 DemTabs <- readRDS("DemocraticNomineePolicies.RDS")
 DemTabNames <- names(DemTabs)
 DemNames <- as.character(DemTabs[[1]]$Candidate)
 DemSR <- as.character(DemTabs[[1]]$`Still running `)
+qbc <- readRDS("qbc.RDS")
 
 
 ui <- shinyUI(
@@ -12,14 +14,22 @@ ui <- shinyUI(
     sidebarLayout(
       sidebarPanel(
         h2("Select Filters"),
-        selectInput("TabNames", "Policy Subject", choices = DemTabNames),
+        uiOutput("tnui"),
         selectInput("Filters", "Select Filter", choices = c("Candidate", "Still Running?"), multiple = TRUE),
         uiOutput("cand"),
         uiOutput("srui")
       ),
       mainPanel(
-        uiOutput("mainHead"),
-        dataTableOutput("demT")
+        tabsetPanel( id = "tabs",
+          tabPanel("By category", value = "t1",
+            uiOutput("mainHead"),
+            dataTableOutput("demT")
+          ),
+          tabPanel("One tab",value = "t2",
+            tableOutput("ktdem")
+          )
+        )
+        
       )
     )
   )
@@ -27,6 +37,12 @@ ui <- shinyUI(
 
 server <- shinyServer(function(input, output){
   output$mainHead <- renderUI({h2(input$TabNames)})
+  
+  output$tnui <- renderUI({
+    if(input$tabs == "t2")TF <- TRUE else TF <- FALSE
+    selectInput("TabNames", "Policy Subject", choices = DemTabNames, multiple = TF)
+      
+  })
   
   output$cand <- renderUI({
     if("Candidate" %in% input$Filters)
@@ -39,15 +55,41 @@ server <- shinyServer(function(input, output){
   })
   
   output$demT <- renderDataTable({
-    
     tab <- DemTabs[[input$TabNames]]
     if(input$Filters == "Candidate" && !is.null(input$candidates))
       tab <- tab[tab$Candidate %in% input$candidates,]
     if(input$Filters == "Still Running?" && !is.null(input$stillRunning))
       tab <- tab[tab$`Still running ` %in% input$stillRunning,]
     tab
-    
   })
+  
+  output$ktdem <- function(){
+    tab <- qbc
+    if( "Candidate" %in% input$Filters && !is.null(input$candidates))
+      tab <- tab[,c(1,2, which(colnames(tab) %in% input$candidates))]
+    if("Still Running?" %in% input$Filters && !is.null(input$stillRunning))
+      tab <- tab[,c(1,2, grep(input$stillRunning, tab[1,]))]
+    if(!is.null(input$TabNames))
+      tab <- tab[tab$Category %in% input$TabNames,]
+    
+    catSum <- cumsum(table(tab$Category))
+    
+    x <- tab %>%
+      select(-Category) %>%
+      kable(escape = F, align = "c", row.names = FALSE)%>%
+      kable_styling(c("striped", "condensed"), full_width = F, fixed_thead = TRUE) %>%
+      pack_rows(names(catSum)[1], 1, catSum[1])
+    
+    for(i in 2:length(catSum)){
+      x <-  x %>%
+        pack_rows(names(catSum)[i], catSum[i-1] + 1, catSum[i])
+    }
+    
+    x
+    
+  }
+  
+
 })
 
 shinyApp(ui = ui, server = server)
